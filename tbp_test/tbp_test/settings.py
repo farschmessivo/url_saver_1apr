044890a5f3e9e77ba32bce77e939c0a0f5746780
Roman Ch <hello@romangagarin.com>
2018-04-01 16:41:30 +0100

    change requirements.txt

--- tbp_test/tbp_test/settings.py
+++ tbp_test/tbp_test/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +127,4 @@ USE_TZ = True
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
